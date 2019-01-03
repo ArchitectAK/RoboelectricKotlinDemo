d113f87fe1a7bf37a2932f81c1d2a087bf0e9031
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-01-03 15:03:12 +0800

    Added first activity

--- app/src/main/java/com/cogitator/roboelectrickotlindemo/FirstActivity.kt
+++ app/src/main/java/com/cogitator/roboelectrickotlindemo/FirstActivity.kt
@@ -1,9 +1,22 @@
 package com.cogitator.roboelectrickotlindemo
 
+import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 /**
  * @author Ankit Kumar on 03/01/2019
  */
 class FirstActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+    }
+
+    fun buttonClickTest(view: View) {
+        val intent = Intent(applicationContext, SecondActivity::class.java)
+        startActivity(intent)
+    }
 }
