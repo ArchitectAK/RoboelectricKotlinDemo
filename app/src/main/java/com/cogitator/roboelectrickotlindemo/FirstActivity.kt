daf4113ec4e8a2b5e0a9d1d4ee33b17949617f91
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-01-03 12:27:46 +0800

    Added first activity

---
+++ app/src/main/java/com/cogitator/roboelectrickotlindemo/FirstActivity.kt
@@ -0,0 +1,9 @@
+package com.cogitator.roboelectrickotlindemo
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author Ankit Kumar on 03/01/2019
+ */
+class FirstActivity : AppCompatActivity() {
+}
