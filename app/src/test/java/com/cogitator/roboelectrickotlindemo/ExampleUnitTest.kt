e4b7aa2124fa7b5157fc9d7990a21613792bc919
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-01-08 14:38:50 +0800

    added test case

--- app/src/test/java/com/cogitator/roboelectrickotlindemo/ExampleUnitTest.kt
+++ app/src/test/java/com/cogitator/roboelectrickotlindemo/ExampleUnitTest.kt
@@ -1,11 +1,21 @@
 package com.cogitator.roboelectrickotlindemo
 
+import android.content.Intent
+import android.widget.Button
+import android.widget.TextView
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.runner.RunWith
+import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.Shadows
+import org.robolectric.Shadows.shadowOf
 import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowActivity
+import org.robolectric.shadows.ShadowIntent
+import org.hamcrest.CoreMatchers.equalTo
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -16,10 +26,33 @@ import org.robolectric.annotation.Config
  */
 
 @RunWith(RobolectricTestRunner::class)
-@Config(constants=BuildConfig::class, packageName = "com.cogitator.roboelectrickotlindemo")
+@Config(constants = BuildConfig::class, packageName = "com.cogitator.roboelectrickotlindemo")
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    private var firstActivity: FirstActivity = null!!
+    @Before
+    fun init() {
+        firstActivity = Robolectric.setupActivity(FirstActivity::class.java)
     }
+
+    @Test
+    fun clickingLoginButton_shouldStartSecondActivity() {
+        val button = firstActivity.findViewById<Button>(R.id.button1)
+
+        assertNotNull(button)
+        button.performClick()
+
+
+        val showActivity: ShadowActivity = Shadows.shadowOf(firstActivity)
+        val intent: Intent = showActivity.nextStartedActivity
+        val shadowIntent: ShadowIntent = shadowOf(intent)
+        assertThat(shadowIntent.intentClass.name, equalTo(FirstActivity::class.java!!.getName()))
+    }
+
+    @Test
+    fun checkTextViewString_presentOrNot() {
+        val textView = firstActivity.findViewById<TextView>(R.id.textView)
+        val stringValue = textView.text.toString()
+        assertThat(stringValue, equalTo("Hello World!"))
+    }
+
 }
