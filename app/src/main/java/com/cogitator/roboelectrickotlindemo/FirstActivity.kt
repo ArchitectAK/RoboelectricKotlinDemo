ddf726552ad54bdf23ac990bfc5f3249064b719d
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-01-08 10:19:05 +0800

    Added first activity

--- app/src/main/java/com/cogitator/roboelectrickotlindemo/FirstActivity.kt
+++ app/src/main/java/com/cogitator/roboelectrickotlindemo/FirstActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * @author Ankit Kumar on 03/01/2019
@@ -13,10 +14,10 @@ class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
-    }
 
-    fun buttonClickTest(view: View) {
-        val intent = Intent(applicationContext, SecondActivity::class.java)
-        startActivity(intent)
+        button1.setOnClickListener {
+            val intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/cogitator/roboelectrickotlindemo/SecondActivity.kt
+++ app/src/main/java/com/cogitator/roboelectrickotlindemo/SecondActivity.kt
@@ -1,9 +1,14 @@
 package com.cogitator.roboelectrickotlindemo
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 /**
  * @author Ankit Kumar on 03/01/2019
  */
 class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
