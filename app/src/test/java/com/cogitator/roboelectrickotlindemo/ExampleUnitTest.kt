481c1d709fc517569357f5774774df886d5ba802
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-01-08 14:28:59 +0800

    added roboelectric dependencies

--- app/src/test/java/com/cogitator/roboelectrickotlindemo/ExampleUnitTest.kt
+++ app/src/test/java/com/cogitator/roboelectrickotlindemo/ExampleUnitTest.kt
@@ -3,12 +3,20 @@ package com.cogitator.roboelectrickotlindemo
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
+ *
+ *
  */
+
+@RunWith(RobolectricTestRunner::class)
+@Config(constants=BuildConfig::class, packageName = "com.cogitator.roboelectrickotlindemo")
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
