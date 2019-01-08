51a5658a3b4b378928210f8c6e1c72917e95472b
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2019-01-08 10:19:58 +0800

    Added second activity

--- app/src/main/java/com/cogitator/roboelectrickotlindemo/SecondActivity.kt
+++ app/src/main/java/com/cogitator/roboelectrickotlindemo/SecondActivity.kt
@@ -9,6 +9,6 @@ import android.support.v7.app.AppCompatActivity
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
     }
 }
